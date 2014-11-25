e118a3e7e9254ab7b4dfc19b0a93747b94612072
trevorjtclarke <trevor@babblr.me>
2014-11-24 17:01:53 -0800

    building out cool moods

--- www/js/controllers/MainCtrl.js
+++ www/js/controllers/MainCtrl.js
@@ -1,7 +1,16 @@
-UP.controller('MainCtrl', function($scope, $state, $ionicModal) {
+UP.controller('MainCtrl', function($scope, $state, $ionicModal, Moods) {
     
     $scope.hello = function () {
         alert('hello');
+    };
+
+    /**
+     * Mood Logic
+     */
+    $scope.moods = Moods;
+    $scope.currentMood = Moods[4]; //normal
+    $scope.changeMood = function ( idx ) {
+        $scope.currentMood = Moods[idx];
     };
 
 });
